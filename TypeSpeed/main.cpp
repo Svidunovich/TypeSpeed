7bfc3555dcaab035ef53028119f7fc848ad9da14
Yaroslav Svidunovich <q77qw2000@gmail.com>
2020-12-04 13:49:06 +0300

    Demo

--- TypeSpeed/main.cpp
+++ TypeSpeed/main.cpp
@@ -1,4 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <functional>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 
 std::string current_time_and_date()
 {
@@ -13,18 +22,17 @@ int main()
 {
 	std::vector<std::string> v = { "->", ",", ".", "-", "'", "[]", "{}", ":", ";", "=", "+", "_", "*", "&", "()", "?", "/", "!", "<" , ">"};
 	std::default_random_engine engine(std::chrono::high_resolution_clock::now().time_since_epoch().count());
-	int i = v.size();
-	std::uniform_int_distribution<int> dist{0, i - 1};
+	std::uniform_int_distribution<int> dist{0, static_cast<int>(v.size()) - 1};
 	auto rr = std::bind(dist, engine);
 	int correct = 0;
 	int wrong = 0;	
-	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+	auto t1 = std::chrono::high_resolution_clock::now();
 	while(1)
 	{
 		system("clear");
 		std::string ra = v[rr()];	
-		std::chrono::high_resolution_clock::time_point t3 = std::chrono::high_resolution_clock::now();
-	   	std::chrono::duration<double> time_span2 = std::chrono::duration_cast<std::chrono::duration<double>>(t3 - t1);
+		auto t3 = std::chrono::high_resolution_clock::now();
+	   	auto time_span2 = std::chrono::duration_cast<std::chrono::duration<double>>(t3 - t1);
 		std::cout << ra << "   Correct: " << correct << "   Wrong: " << wrong << "   Time: " << time_span2.count() << "   |   FullTime: 60" <<  std::endl;
 		std::string str;
 		std::cin >> str;
@@ -36,8 +44,8 @@ int main()
 		{
 			wrong++;
 		}	
-		std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+		auto t2 = std::chrono::high_resolution_clock::now();
+		auto time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
 		if(time_span.count() > 60)
 		{
 			std::cout << "Time out!" << std::endl;
@@ -48,7 +56,7 @@ int main()
 	std::ofstream file;
 	file.open("Data.txt", std::ios_base::app);
 	std::stringstream ss;
-	ss << "Correct: " << correct << "   |   Wrong: " << wrong << "   |   Data time: " <<  current_time_and_date();
+	ss << "Correct: " << correct << "   |   Wrong: " << wrong << "   |   Data time: " <<  current_time_and_date() << "\n";
 	file << ss.str();
 	file.close();
 	return 0;
